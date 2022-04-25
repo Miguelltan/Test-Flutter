14cc51e20aa093a820c24591559f155d24f61209
Miguelltan <Miguelltan18@gmail.com>
2022-04-25 08:20:49 +0700

    first commit

---
+++ sampleproject/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child:Column(children: [
+             Image(
+            image: NetworkImage(
+              'https://cdn.pixbay.com/photo/2016/04/23/22/00/tree-736885__340.jpg'
+            ),
+          ),
+          Text('Hello World')
+          ],)
+        ),
+      ),
+      
+    );
+  }
+}
+
